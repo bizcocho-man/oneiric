09e74b2db4502693109c726a4aaf6473413ba255
pakume <fpuertasrodriguez@gmail.com>
2018-11-15 20:25:04 +0100

    - Added PlayerController and CheckCollision with ground

---
+++ Assets/Scripts/CheckCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour
+{
+    [SerializeField] PlayerController PlayerController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController.IsOnGround = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        PlayerController.IsOnGround = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerController.IsOnGround = false;
+    }
+}
