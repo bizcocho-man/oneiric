b7aa542fb991e0bfe4af301c9f13e34bd7b5438c
Juan Escudero <shielderjohn@gmail.com>
2018-11-25 20:48:16 +0100

    - New woodbox mesh - Added VFXs (smoke, sparks) (Remember to give credit to unity!)

---
+++ Assets/VFX/Sparks/VFX_ElectricalSparks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFX_ElectricalSparks : MonoBehaviour
+{
+    public float minWaitTime = 2.0f;
+    public float maxWaitTime = 5.0f;
+
+    private ParticleSystem particles;
+
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        StartCoroutine(RandomSparks());
+    }
+
+    private IEnumerator RandomSparks()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+
+            if (particles != null)
+            {
+                particles.Play();
+            }
+        }
+    }
+}
