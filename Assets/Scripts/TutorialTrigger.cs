fb0b0a26aadfce9cfce52fa27b6e932c972c4371
Juan Escudero <shielderjohn@gmail.com>
2018-12-01 10:07:06 +0100

    - Scene 1 and 2 baked

---
+++ Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    [SerializeField] private Canvas tutorialCanvas;
+    [SerializeField] private Text text1;
+    [SerializeField] private Image keyboardButton;
+    [SerializeField] private Text separator;
+    [SerializeField] private Image gamepadButton;
+    [SerializeField] private Text text2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        tutorialCanvas.enabled = true;
+    }
+}
